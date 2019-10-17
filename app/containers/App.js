0eaf98e8ee9417dce42a737b2a1bfce90f081f18
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-10-17 09:28:25 +0200

    cleaning the boilerplate

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,10 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
-import MainSection from '../components/MainSection';
 import * as TodoActions from '../actions/todos';
-import style from './App.css';
 
 @connect(
   state => ({
@@ -23,11 +20,9 @@ export default class App extends Component {
 
   render() {
     const { todos, actions } = this.props;
-
     return (
-      <div className={style.normal}>
-        <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
+      <div className="content">
+        <p>Here comes content</p>
       </div>
     );
   }
