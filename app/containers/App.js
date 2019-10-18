474b00e86761adfe91c2870f084a2927aecd5d40
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-10-18 12:59:58 +0200

    Closures are ugly but doing the job. Direstories extraction works (for now console.log)

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,7 +1,7 @@
-import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as TodoActions from '../actions/todos';
+import React, { Component, PropTypes } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as TodoActions from "../actions/todos";
 
 @connect(
   state => ({
@@ -17,11 +17,52 @@ export default class App extends Component {
     actions: PropTypes.object.isRequired
   };
 
+  // Bookmarks tree specific
+  // dirs will change to prop
+  constructor() {
+    super();
+    this.state = {
+      dirs: []
+    };
+  }
+
+  componentDidMount(){
+    this.extractDirs()
+  }
+
+  extractDirs(){
+
+    function hasChildren(node) {
+      return (
+        typeof node === "object" &&
+        typeof node.children !== "undefined" &&
+        node.children.length > 0
+      );
+    }
+
+    const extractDirsRecursion = (id = '0') => {
+      let bookmarksDir = chrome.bookmarks.getSubTree(id, results => {
+        for(let entry of results){
+          if(hasChildren(entry)){
+            recArr.push(entry.title)
+            for(let child of entry.children){
+              extractDirsRecursion(child.id)
+            }
+          }
+        }
+      })
+    }
+    var recArr = [];
+    extractDirsRecursion()
+    console.log(recArr)
+    this.setState({dirs : recArr})
+  }
+
   render() {
     const { todos, actions } = this.props;
     return (
       <div className="content">
-        <p>Here comes content</p>
+        <p>{this.state.dirs}</p>
       </div>
     );
   }
